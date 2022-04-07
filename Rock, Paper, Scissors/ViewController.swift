7d6eeebfc5adfa366eda4fca0f8de5a0af657dd0
Brendon Zach <brendon_zach@outlook.com>
2022-04-07 14:07:52 -0600

    began mvp

--- Rock, Paper, Scissors/ViewController.swift
+++ Rock, Paper, Scissors/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scissors: UIImageView!
+    @IBOutlet weak var theRock: UIImageView!
+    @IBOutlet weak var paper: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
